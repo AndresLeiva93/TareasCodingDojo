434d4196e4ab2ed7257f8b4aa2436469797181d5
AndresLeiva93 <89427059+AndresLeiva93@users.noreply.github.com>
2021-08-30 19:53:16 -0400

    Metodos con Range

---
+++ Range/range.rb
@@ -0,0 +1,14 @@
+#Metodos con Range
+rango = (1..5)
+
+#Si incluye o no una variable en el rango
+puts rango.include?(4)
+
+#Ultimo elemento del rango
+puts rango.last 
+
+#Maximo elemento en el rango
+puts rango.max 
+
+#Minomo elemento en el rango
+puts rango.min
