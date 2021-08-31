6f7c6e3d12328ce06d120c19c6b122d58489ebd1
AndresLeiva93 <89427059+AndresLeiva93@users.noreply.github.com>
2021-08-30 20:37:48 -0400

    Muestre la suma

--- TreceBasicos/TreceBasicos.rb
+++ TreceBasicos/TreceBasicos.rb
@@ -1,5 +1,5 @@
 #Actividad 13 basicos
-
+=begin
 #Mostrar 1-255
 for i in 1..255
     puts i
@@ -7,3 +7,12 @@ end
 
 #Mostrar numeros impares entre 1 y 255
 puts (1..255).to_a.reject { |elem| elem.even? }
+=end
+
+#Muestre la suma
+suma = 0
+for i in 0..255
+    suma += i
+    puts "Nuevo numero: #{i} Suma: #{suma}" 
+end
+
