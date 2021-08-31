578c2af5a5dcca1ade6d960d235be50fbd7adb13
AndresLeiva93 <89427059+AndresLeiva93@users.noreply.github.com>
2021-08-30 20:47:21 -0400

    Recorriendo un arreglo

--- TreceBasicos/TreceBasicos.rb
+++ TreceBasicos/TreceBasicos.rb
@@ -7,7 +7,6 @@ end
 
 #Mostrar numeros impares entre 1 y 255
 puts (1..255).to_a.reject { |elem| elem.even? }
-=end
 
 #Muestre la suma
 suma = 0
@@ -15,4 +14,13 @@ for i in 0..255
     suma += i
     puts "Nuevo numero: #{i} Suma: #{suma}" 
 end
+=end
+
+#Recorriendo un arreglo
+for i in [1,4,2,6]
+    puts i
+end
+
+
+
 
